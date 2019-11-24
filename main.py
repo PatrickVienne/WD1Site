0c4ae6e56c0f319800fd1d48ed5a0ca12a69435a
pfo <pfo@visotech.com>
2019-11-24 17:31:34 +0100

    TEST

--- main.py
+++ main.py
@@ -2,21 +2,43 @@ import flask
 import random
 import model
 
-from flask import url_for
+import string
 
 app = flask.Flask(__name__)
 db = model.db
 
-
 db.create_all()
+
+
+def add_example_users():
+    for x in range(4):
+        name = "".join([x for x in random.choices(string.ascii_letters, k=8)]).capitalize()
+        user = model.User(username=name,
+                          email=f"{name}@haus.at")
+        # only add if it does not exist already
+        if not db.query(model.User).filter_by(email=user.email).first():
+           db.add(user)
+    db.commit()
+
+
+def add_example_receipes():
+    receipe_1 = model.Receipe("Apfelstrudel", "Cut Apple Bake Sweet")
+    receipe_2 = model.Receipe("Hamburger", "Fry Meat And Eat")
+    receipe_3 = model.Receipe("Suppe", "Cut carrots Add Water")
+    # only add if it does not exist already
+    db.add_all([x for x in (receipe_1, receipe_2, receipe_3)
+                if not db.query(model.Receipe).filter_by(name=x.name).first()
+                ])
+    db.commit()
+
+
+def add_example_data():
+    add_example_users()
+    add_example_receipes()
 
 
 @app.route("/")
 def index():
-    # user = model.User(id=1, username="Wolfi", email="wolfi@haus.at")
-    # db.add(user)
-    # db.commit()
-
     return flask.render_template("index.html", myname="Patrick")
 
 
@@ -33,13 +55,9 @@ def secret_number_game():
 
 @app.route("/blog")
 def blog():
-
-    receipe_1 = model.Receipe("Apfelstrudel", "Cut Apple Bake Sweet")
-    receipe_2 = model.Receipe("Hamburger", "Fry Meat And Eat")
-    receipe_3 = model.Receipe("Suppe", "Cut carrots Add Water")
-
-    return flask.render_template("blog.html", receipes=[receipe_1, receipe_2, receipe_3])
+    return flask.render_template("blog.html", receipes=db.query(model.Receipe).all())
 
 
 if __name__ == '__main__':
+    add_example_data()
     app.run()
--- model.py
+++ model.py
@@ -1,17 +1,37 @@
 import sqla_wrapper
 import os
 
+# run sqlite only in memory, then all data is destroyed on application restart.
+#SQLITE_FILE = ':memory:'
 
-db = sqla_wrapper.SQLAlchemy(os.getenv("DATABASE_URL", "sqlite:///localhost.sqlite"))
+# save all data in a temporary file so it can be inspected
+SQLITE_FILE = 'localhost.sqlite'
+db = sqla_wrapper.SQLAlchemy(os.getenv("DATABASE_URL", f"sqlite:///{SQLITE_FILE}"))
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True)
-    email = db.Column(db.String, unique=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email, **kwargs):
+        self.username = username
+        self.email = email
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        return f'User[username={self.username}, email={self.email}]'
 
 
-class Receipe:
-    def __init__(self, name, description):
+class Receipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True)
+    description = db.Column(db.Text(4000), unique=True)
+
+    def __init__(self, name, description, **kwargs):
         self.name = name
         self.description = description
+        super().__init__(**kwargs)
+
+    def __repr__(self):
+        return f'Receipe[name={self.name}, description={self.description}]'
